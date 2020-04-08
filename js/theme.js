a73344c3316da67b9bf818a6f299e1721596dafe
Prasanna Kumar <prasannakumar027@gmail.com>
2020-04-08 19:44:34 +0530

    new plugins and mods

--- js/theme.js
+++ js/theme.js
@@ -74,9 +74,24 @@
             }
         }]
     });
-   
+   	var latitude = $('#google-map').data('latitude');
+	var longitude = $('#google-map').data('longitude');
+	function initialize_map() {
+		var myLatlng = new google.maps.LatLng(latitude,longitude);
+		var mapOptions = {
+			zoom: 14,
+			scrollwheel: false,
+			center: myLatlng
+		};
+		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map
+		});
+	}
+	google.maps.event.addDomListener(window, 'load', initialize_map);
 
-          /*START GOOGLE MAP*/
+          /*START GOOGLE MAP
           function initialize() {
             var mapOptions = {
               zoom: 15,
@@ -92,7 +107,7 @@
             });
           }
           google.maps.event.addDomListener(window, 'load', initialize);	
-          /*END GOOGLE MAP*/	
+          */	
 
  
 
