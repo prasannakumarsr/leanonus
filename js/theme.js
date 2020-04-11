17ce1440cde0d298581a7fa5f3f3a02a28ab3852
Prasanna Kumar <prasannakumar027@gmail.com>
2020-04-11 21:42:14 +0530

    new logos and favicon

--- js/theme.js
+++ js/theme.js
@@ -74,6 +74,27 @@
             }
         }]
     });
+	
+	$('.banner-slide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
    	var latitude = $('#google-map').data('latitude');
 	var longitude = $('#google-map').data('longitude');
 	function initialize_map() {
