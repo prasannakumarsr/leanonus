c398b75a91b689931c5cb4d9bda28b72425bbdf0
Prasanna Kumar <prasannakumar027@gmail.com>
2020-04-14 18:46:32 +0530

    bug fix

--- js/theme.js
+++ js/theme.js
@@ -79,7 +79,7 @@
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1500,
+        autoplaySpeed: 20000,
         arrows: false,
         dots: false,
         pauseOnHover: false,
